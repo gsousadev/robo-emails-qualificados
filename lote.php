4c303836ad5c022ab670ed09db3759000bcf30cf
Guilherme Santos <gsousadev@gmail.com>
2023-12-02 00:16:40 -0300

    fix: Ajustando busca de arquivos json de input

--- lote.php
+++ lote.php
@@ -5,16 +5,16 @@ require 'src/getPlacesByTextSearch.php';
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
 $dotenv->load();
 
-$categorias = json_decode('./input_files/categorias.json', true);
+$categorias = json_decode(file_get_contents('./input_files/categorias.json'), true);
 
-$regioes = json_decode('./input_files/regioes.json', true);
+$regioes = json_decode(file_get_contents('./input_files/regioes.json'), true);
 
 $count = 1;
 
-foreach($categorias as $categoria){
-   foreach($regioes as $regiao){
-        echo "- Iniciando Busca número: $count" . PHP_EOL; 
-        echo "-- Termo de pesquisas: $categoria $regiao" . PHP_EOL; 
+foreach ($categorias as $categoria) {
+    foreach ($regioes as $regiao) {
+        echo "- Iniciando Busca número: $count" . PHP_EOL;
+        echo "-- Termo de pesquisas: $categoria $regiao" . PHP_EOL;
         initSearch($regiao, $categoria);
         $count++;
         echo PHP_EOL;
